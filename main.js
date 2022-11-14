1e89836442d4aa1f43f8903837439c4b574376c8
sophiab411 <sophiabarber411@gmail.com>
2022-11-14 16:43:52 -0500

    Update JS

---
+++ main.js
@@ -0,0 +1,21 @@
+const items = ['🍀', '🌈', '🥇'];
+
+const luckRulesLookUp = { 
+    fourLeafClover: {
+        image: '🍀';
+        triple: 'wins'
+    },
+    rainbow: {
+        image: '🌈';
+        triple: 'wins'
+    },
+    gold: {
+        image: '🥇',
+        triple: 'wins'
+    }
+
+}
+const potsOfGold = document.querySelectorAll('.potOfGold');
+
+
+  
